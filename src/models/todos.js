b37b74a4938ba2075c21b16a9f57e714a57d7a6b
M. Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2022-12-08 21:26:48 +0800

    add relation 'one to many'

--- src/models/todos.js
+++ src/models/todos.js
@@ -1,28 +1,30 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Todos extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Todos extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            this.belongsTo(models.Todos, {
+                foreignKey: "activity_group_id",
+            });
+        }
     }
-  }
-  Todos.init(
-    {
-      activity_group_id: DataTypes.INTEGER,
-      title: DataTypes.STRING,
-      is_active: DataTypes.STRING,
-      priority: DataTypes.STRING
-    },
-    {
-      sequelize,
-      paranoid: true,
-      modelName: "Todos"
-    }
-  );
-  return Todos;
+    Todos.init(
+        {
+            activity_group_id: DataTypes.INTEGER,
+            title: DataTypes.STRING,
+            is_active: DataTypes.STRING,
+            priority: DataTypes.STRING,
+        },
+        {
+            sequelize,
+            paranoid: true,
+            modelName: "Todos",
+        }
+    );
+    return Todos;
 };
