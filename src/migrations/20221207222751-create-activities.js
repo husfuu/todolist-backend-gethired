d94f8422c1a25182cb121638cfdc84c92d49cc1e
M. Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2022-12-09 08:09:41 +0800

    change the name of tables

--- src/migrations/20221207222751-create-activities.js
+++ src/migrations/20221207222751-create-activities.js
@@ -2,33 +2,18 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable("todos", {
+        await queryInterface.createTable("activities", {
             id: {
                 allowNull: false,
                 autoIncrement: true,
                 primaryKey: true,
                 type: Sequelize.INTEGER,
             },
-            activity_group_id: {
-                allowNull: false,
-                type: Sequelize.INTEGER,
-                references: {
-                    model: {
-                        tableName: "activities",
-                    },
-                    key: "id",
-                },
-            },
-            title: {
+            email: {
                 allowNull: false,
                 type: Sequelize.STRING,
             },
-            is_active: {
-                allowNull: false,
-                type: Sequelize.BOOLEAN,
-                defaultValue: true,
-            },
-            priority: {
+            title: {
                 allowNull: false,
                 type: Sequelize.STRING,
             },
@@ -46,6 +31,6 @@ module.exports = {
         });
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable("todos");
+        await queryInterface.dropTable("activities");
     },
 };
--- src/models/activities.js
+++ src/models/activities.js
@@ -1,30 +1,28 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    class todos extends Model {
+    class activities extends Model {
         /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            this.belongsTo(models.todos, {
+            this.hasMany(models.todos, {
                 foreignKey: "activity_group_id",
             });
         }
     }
-    todos.init(
+    activities.init(
         {
-            activity_group_id: DataTypes.INTEGER,
+            email: DataTypes.STRING,
             title: DataTypes.STRING,
-            is_active: DataTypes.BOOLEAN,
-            priority: DataTypes.STRING,
         },
         {
             sequelize,
             paranoid: true,
-            modelName: "todos",
+            modelName: "activities",
         }
     );
-    return todos;
+    return activities;
 };
